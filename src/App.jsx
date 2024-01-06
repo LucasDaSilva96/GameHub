a40d55003df081e9b2b659813b242c3ad948e520
LucasDaSilva96 <dasilvajunior881@gmail.com>
2024-01-06 12:14:51 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
